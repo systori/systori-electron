9850142e91bedfc407c75f73856794257b99dd39
Marius Räsener <marius@raesener.de>
2020-02-11 13:45:10 +0100

    WIP

--- src/main/index.js
+++ src/main/index.js
@@ -1,35 +1,130 @@
 'use strict'
 
-import { app, BrowserWindow, session } from 'electron'
+import { app, BrowserWindow, session, screen, Menu } from 'electron'
 import * as path from 'path'
 import { format as formatUrl } from 'url'
 
 const Store = require('electron-store');
 const store = new Store();
-const isDevelopment = process.env.NODE_ENV !== 'production'
+const isDevelopment = process.env.NODE_ENV !== 'production';
+const contextMenu = require('electron-context-menu');
+const openAboutWindow = require('about-window').default;
+
+contextMenu({
+	labels: {
+    copy: 'Kopieren',
+    paste: 'Einfügen',
+    cut: 'Ausschneiden',
+    copyLink: 'Link kopieren',
+  },
+  showLookUpSelection: false,
+});
+
+
+// Window Menu
+const menu = Menu.buildFromTemplate([
+  // { role: 'appMenu' }
+  ...(process.platform === 'darwin' ? [{
+      label: app.getName(),
+      submenu: [
+          { role: 'about' },
+          { type: 'separator' },
+          { role: 'services' },
+          { type: 'separator' },
+          { role: 'hide' },
+          { role: 'hideothers' },
+          { role: 'unhide' },
+          { type: 'separator' },
+          { role: 'quit' }
+      ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+      label: 'Datei',
+      submenu: [{
+              label: 'Neu',
+              click: function(menuItem, currentWindow) {
+                  if (currentWindow)
+                      currentWindow.webContents.send('new');
+              },
+              accelerator: 'Ctrl+N'
+          },
+          {
+              type: 'separator'
+          },
+          {
+              label: 'Export',
+              submenu: [{
+                      label: 'als JPEG',
+                      click: function(menuItem, currentWindow) {
+                          if (currentWindow)
+                              currentWindow.webContents.send('has-color-stamp-exportjpeg');
+                      },
+                      accelerator: 'Ctrl+Shift+J'
+                  },
+                  {
+                      label: 'als PDF',
+                      click: function(menuItem, currentWindow) {
+                          if (currentWindow)
+                              currentWindow.webContents.send('has-color-stamp-exportpdf');
+                      },
+                      accelerator: 'Ctrl+Shift+P'
+                  }
+              ]
+          },
+          {
+              type: 'separator'
+          },
+          {
+              label: 'Beenden',
+              click: () =>
+                  app.quit(),
+          }
+      ]
+  },
+  // { role: 'editMenu' }
+  {
+      label: 'Bearbeiten',
+      submenu: [{
+              label: 'Zurück',
+              click: function(menuItem, currentWindow) {
+                  if (currentWindow)
+                      currentWindow.webContents.send('back');
+              },
+              accelerator: 'Ctrl+Z'
+          },
+          {
+              label: 'Wiederherstellen',
+              click: function(menuItem, currentWindow) {
+                  if (currentWindow)
+                      currentWindow.webContents.send('redo');
+              },
+              accelerator: 'Ctrl+Y'
+          },
+      ]
+  },
+]);
 
 // global reference to mainWindow (necessary to prevent window from being garbage collected)
 let mainWindow
 
 function createMainWindow() {
-  const window = new BrowserWindow({webPreferences: {nodeIntegration: true}})
+  
+  const height = store.get("height") ?? 800;
+  const width = store.get("width") ?? 1260;
+  const x = store.get("x") ?? 0;
+  const y = store.get("y") ?? 0;
+  const window = new BrowserWindow({
+    width: width, height: height, x: x, y: y
+  })
+  
+  if (isDevelopment) {
+    window.webContents.openDevTools()
+  }
+  Menu.setApplicationMenu(menu);
 
-  // if (isDevelopment) {
-  //   window.webContents.openDevTools()
-  // }
-
-  // if (isDevelopment) {
-  //   window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
-  // }
-  // else {
-  //   window.loadURL(formatUrl({
-  //     pathname: path.join(__dirname, 'index.html'),
-  //     protocol: 'file',
-  //     slashes: true
-  //   }))
-  // }
-
-  window.loadURL("https://app.systori.com")
+  const url = store.get("url") ?? "https://app.systori.com";
+  window.loadURL(url)
   // session.defaultSession.cookies.get({url: 'http://www.github.com'}, (error, cookies) => {
   //   console.log(error, cookies)
   // })
@@ -50,9 +145,20 @@ function createMainWindow() {
 // quit application when all windows are closed
 app.on('window-all-closed', () => {
   // on macOS it is common for applications to stay open until the user explicitly quits
+  const bounds = mainWindow.getBounds();
+  store.set({height: bounds.height, width:bounds.width, x: bounds.x, y: bounds.y})
+  const url = mainWindow.getURL();
+  store.set({url: url});
   if (process.platform !== 'darwin') {
     app.quit()
   }
+})
+
+app.on('before-quit', () => {
+  const bounds = mainWindow.getBounds();
+  store.set({height: bounds.height, width:bounds.width, x: bounds.x, y: bounds.y})
+  const url = mainWindow.getURL();
+  store.set({url: url});
 })
 
 app.on('activate', () => {
